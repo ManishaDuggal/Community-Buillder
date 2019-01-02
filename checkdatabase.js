0f00827cc08119cb921ce4875a8e472b1e02b78b
manu99999 <34620776+manu99999@users.noreply.github.com>
2019-01-02 19:27:43 +0530

    Delete checkdatabase.js

--- checkdatabase.js
+++
@@ -1,9 +0,0 @@
-var i=require('./users-database');
-
-i.getUsers(function(result){
- // console.log(result);
-});
-
-                var mailer=require('./mail.js');
-                mailer.sendmail('manuduggal9@gmail.com','again huh!');
-                
